6478111673335fc334a8540c6d08a36eaef78104
saajan <saajan.sn@gmail.com>
2016-06-22 01:18:37 +0530

    vendor js edit jquery and mordernizr

--- gulpFile.js
+++ gulpFile.js
@@ -72,7 +72,10 @@ gulp.task('scripts-custom', function() {
 
 // Scripts
 gulp.task('scripts-vendor', function() {
-  return gulp.src(['src/scripts/vendor/*.js'])
+  return gulp.src([
+    'src/scripts/vendor/jquery-3.0.0.js',
+    'src/scripts/vendor/mordernizr-3.0.1.js'
+  ])
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('public/dist/scripts'))
     .pipe(rename({
